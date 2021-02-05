090b39e90ebae808032c9b2d54db91ae0b38ac08
David Nisbet <70908860+Nizz3hx@users.noreply.github.com>
2021-02-05 18:00:57 +0000

    Completed challenge.

--- Challenge1/main.js
+++ Challenge1/main.js
@@ -1,1 +1,8 @@
 // Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down.
+
+
+function takesTwoNums(num1, num2) {
+  console.log(Math.floor(num1 / num2))
+}
+
+takesTwoNums(150,9);
