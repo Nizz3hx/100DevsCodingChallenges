c7d216c5467ac62e3da234e8b13109c1e556852b
David Nisbet <70908860+Nizz3hx@users.noreply.github.com>
2021-02-07 16:33:21 +0000

    Completed Daily Challenge! :D

---
+++ Challenge3/main.js
@@ -0,0 +1,9 @@
+//Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+
+
+function takeArr(arr){
+  console.log(arr[0] + arr[arr.length - 1])
+}
+
+
+takeArr([1,2,3,4,5,6,7,45])
